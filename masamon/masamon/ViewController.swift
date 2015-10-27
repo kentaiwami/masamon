96fe111fd8a9279cef8208d0937214e43d35c070
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-27 15:39:37 +0900

    Menuボタンの追加

--- masamon/masamon/ViewController.swift
+++ masamon/masamon/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "../images/Menu-50.png")! as UIImage
+        let imageButton   = UIButton()
+        imageButton.tag = 0
+        imageButton.frame = CGRectMake(0, 0, 128, 128)
+        imageButton.layer.position = CGPoint(x: self.view.frame.width-30, y:60)
+        imageButton.setImage(image, forState: .Normal)
+        imageButton.addTarget(self, action: "MenuButtontapped:", forControlEvents:.TouchUpInside)
+        
+        self.view.addSubview(imageButton)
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func MenuButtontapped(sender: UIButton){
+        print("MenuButtonTapped")
+    }
 
 }
 
