bbb295c9641dbdb03fc53491d53d7cde18f53233
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-27 14:52:24 +0900

    xcodeプロジェクトの追加

---
+++ masamon/masamon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2015/10/27.
+//  Copyright © 2015年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
