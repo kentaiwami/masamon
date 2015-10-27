ccba848bb69f1a0d3b42bfc1456448d8d9a31d8f
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-27 20:42:47 +0900

    変数名を変更

--- masamon/masamon/DB.swift
+++ masamon/masamon/DB.swift
@@ -10,19 +10,19 @@ import RealmSwift
 import Foundation
 
 class ShiftDB: Object {
-    dynamic var ID = 0
-    dynamic var Name = ""       //ユーザが入力した名前を記録
-    dynamic var ImagePath = ""  //取り込んだイメージの保存パスを記録
-    dynamic var Saraly = 0      //取り込んだシフトの月給を記録
-    let ShiftDetails = List<ShiftDetailDB>()
+    dynamic var id = 0
+    dynamic var name = ""       //ユーザが入力した名前を記録
+    dynamic var imagepath = ""  //取り込んだイメージの保存パスを記録
+    dynamic var saraly = 0      //取り込んだシフトの月給を記録
+    let shiftdetails = List<ShiftDetailDB>()
     
     override class func primaryKey() -> String {
-        return "ID"
+        return "id"
     }
 }
 
 class ShiftDetailDB: Object {
-    dynamic var ID = 0
+    dynamic var id = 0
     dynamic var date = ""       //日付のみ記録
     dynamic var staff = ""      //例えば、Aさんが早番、Bさんが遅番、Cさんが公休、Dさんが早番の場合は"A1,B3,D1"となる予定
     dynamic var user = ""       //userのシフトを記録
