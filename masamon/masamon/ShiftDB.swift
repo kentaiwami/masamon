d6cbbbc9f5456a4bbcf76d9803e60ad2248856d4
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-27 15:55:07 +0900

    Realmの追加

---
+++ masamon/masamon/ShiftDB.swift
@@ -0,0 +1,14 @@
+//
+//  ShiftDB.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2015/10/27.
+//  Copyright © 2015年 Kenta. All rights reserved.
+//
+
+import RealmSwift
+
+class ShiftDB: Object {
+    
+
+}
