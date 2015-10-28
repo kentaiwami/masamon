9311b419f6ee8c50afa821ee653a44bcf3e41e32
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-28 21:23:48 +0900

    時給設定の画面を追加

---
+++ masamon/masamon/HourlyPaySetting.swift
@@ -0,0 +1,22 @@
+//
+//  HourlyPaySetting.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2015/10/28.
+//  Copyright © 2015年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class HourlyPaySetting: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
