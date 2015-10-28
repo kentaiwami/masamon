2177e21c850332a372731afcf1ae1a0c85eca98f
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-28 23:40:46 +0900

    時給を入れるフィールドを2つ配置

--- masamon/masamon/HourlyPaySetting.swift
+++ masamon/masamon/HourlyPaySetting.swift
@@ -13,8 +13,9 @@ import UIKit
 
 class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
 
+    @IBOutlet weak var SalalyLabel1: UITextField!
+    @IBOutlet weak var SalalyLabel2: UITextField!
     var myUIPicker1: UIPickerView = UIPickerView()
-    var myUIPicker2: UIPickerView = UIPickerView()
     let timefrom: [String] = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
     let timeto: [String] = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
     let line: [String] = ["〜"]
@@ -26,6 +27,9 @@ class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataS
         myUIPicker1.delegate = self
         myUIPicker1.dataSource = self
         self.view.addSubview(myUIPicker1)
+        
+        SalalyLabel1.keyboardType = .Default
+        SalalyLabel2.keyboardType = .Default
     }
 
     override func didReceiveMemoryWarning() {
