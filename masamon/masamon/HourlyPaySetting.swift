dcd0a79c099af0e7260686e77ba5b7572c140d22
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-29 01:24:08 +0900

    textfieldに反映させた

--- masamon/masamon/HourlyPaySetting.swift
+++ masamon/masamon/HourlyPaySetting.swift
@@ -6,19 +6,18 @@
 //  Copyright © 2015年 Kenta. All rights reserved.
 //
 //TODO: Textfieldを4つ配置する
-//TODO: Textfieldをタップしたらpickerviewが下から出てくる感じにする
 //TODO: 時給をデータベースに格納
 
 import UIKit
 
 class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
 
-    @IBOutlet weak var TEST: UITextField!
+    @IBOutlet weak var TimeFrom1: UITextField!
+    @IBOutlet weak var TimeTo1: UITextField!
     @IBOutlet weak var SalalyLabel1: UITextField!
     @IBOutlet weak var SalalyLabel2: UITextField!
     var myUIPicker1: UIPickerView = UIPickerView()
-    let timefrom: [String] = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
-    let timeto: [String] = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
+    let time: [String] = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
     let line: [String] = ["〜"]
     
     override func viewDidLoad() {
@@ -46,12 +45,11 @@ class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataS
         myUIPicker1.frame = CGRectMake(0,0,self.view.bounds.width/2+20, 260.0)
         myUIPicker1.delegate = self
         myUIPicker1.dataSource = self
-        //self.view.addSubview(myUIPicker1)
         
         SalalyLabel1.keyboardType = .Default
         SalalyLabel2.keyboardType = .Default
-        TEST.inputView = myUIPicker1
-        TEST.inputAccessoryView = toolBar
+        TimeFrom1.inputView = myUIPicker1
+        TimeFrom1.inputAccessoryView = toolBar
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,22 +64,22 @@ class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataS
     //表示個数
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if(component == 0){
-            return timefrom.count
+            return time.count
         }else if(component == 1){
             return line.count
         }else{
-            return timeto.count
+            return time.count
         }
     }
     
     //表示内容
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if(component == 0){
-            return timefrom[row]
+            return time[row]
         }else if(component == 1){
             return line[row]
         }else{
-            return timeto[row]
+            return time[row]
         }
     }
     
@@ -89,6 +87,13 @@ class HourlyPaySetting: UIViewController,UIPickerViewDelegate, UIPickerViewDataS
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         //        print("列: \(row)")
         //        print("値: \(shiftlist[row])")
+        
+        if(component == 0){
+            TimeFrom1.text = time[row]
+        }else if(component == 2){
+            TimeTo1.text = time[row]
+
+        }
     }
     
     //幅を変更
