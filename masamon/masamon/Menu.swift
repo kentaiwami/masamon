5ecdd2cc50ab7b29d4405cf3afcd639c4ec7e5e3
Kenta Iwami <b1013068@fun.ac.jp>
2015-10-30 01:20:15 +0900

    buttonも動くことを確認

--- masamon/masamon/Menu.swift
+++ masamon/masamon/Menu.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class Menu: UIViewController {
+    let AnimationMenuView = UIView(frame: CGRectMake(345, 60, 100, 100))
+    let aaaaaa = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        aaaaaa.frame = CGRectMake(0, 0, 30, 30)
+        aaaaaa.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        self.AnimationMenuView.addSubview(aaaaaa)
+        self.view.addSubview(AnimationMenuView)
         //メニューボタンの追加
         let image = UIImage(named: "../images/Menu-50.png")! as UIImage
         let imageButton   = UIButton()
@@ -28,7 +34,12 @@ class Menu: UIViewController {
     }
     
     func MenuButtontapped(sender: UIButton){
-        print("aaaa")
+        
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.AnimationMenuView.backgroundColor = UIColor.greenColor()
+            self.AnimationMenuView.frame = CGRectMake(self.view.frame.width/2-250, self.view.frame.height/2-150, 500, 300)
+        })
     }
     
 }
