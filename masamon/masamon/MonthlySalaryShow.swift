445d3084a2a39cb90d94e96cdb4518d96fb42d91
Kenta Iwami <b1013068@fun.ac.jp>
2015-11-04 09:52:56 +0900

    ビューコントローラーの名前を変更

--- masamon/masamon/MonthlySalaryShow.swift
+++ masamon/masamon/MonthlySalaryShow.swift
@@ -15,7 +15,7 @@
 import UIKit
 import RealmSwift
 
-class ViewController: Menu,UIPickerViewDelegate, UIPickerViewDataSource{
+class MonthlySalaryShow: Menu,UIPickerViewDelegate, UIPickerViewDataSource{
     
     let shiftdb = ShiftDB()
     let shiftdetaildb = ShiftDetailDB()
