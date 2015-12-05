62a2f329ce7d99f8dfafaac88ce72f3451027fbb
Kenta Iwami <b1013068@fun.ac.jp>
2015-12-05 20:34:49 +0900

    ライブラリ追加

---
+++ masamon/XlsxReaderWriter-master/XlsxReaderWriter/UIColor+OpenXML.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+OpenXml.h
+//  Levé
+//
+//  Created by René BIGOT on 14/04/2014.
+//  Copyright (c) 2014 René Bigot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIColor+HTML.h"
+
+@interface UIColor (OpenXml)
+
++ (NSArray *)defaultIndexedColors;
+
+@end
