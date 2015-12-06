a3c9ccae6f26aac7a4a2a31885c1709d83502dd7
Kenta Iwami <b1013068@fun.ac.jp>
2015-12-06 21:13:54 +0900

    スタッフ名が含まれるセル位置をarrayで返す関数を実装

--- masamon/masamon/ShiftRegister.swift
+++ masamon/masamon/ShiftRegister.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ShiftRegister: UIViewController {
 
     //
     func AAA(importname: String, importpath: String){
         let shiftdb = ShiftDB()
-        let shitdetaildb = ShiftDetailDB()
+        let shiftdetaildb = ShiftDetailDB()
+        let documentPath: String = NSBundle.mainBundle().pathForResource("bbb", ofType: "xlsx")!
+        let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(documentPath)
+        let worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
+
         
         //shiftDBへidの追加
         if(DBmethod().DBRecordCount(ShiftDB) == 0){
@@ -24,18 +29,60 @@ class ShiftRegister: UIViewController {
         
         shiftdb.shiftimportname = importname
         shiftdb.shiftimportpath = importpath
+//        shiftdb.shiftdetail = 
         
-        let documentPath: String = NSBundle.mainBundle().pathForResource("bbb", ofType: "xlsx")!
-        let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(documentPath)
-        let worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
+        var date = 11
+        var abc = List<ShiftDetailDB>()
         
-        var formula: String = worksheet.cellForCellReference("F6").stringValue()
+        for(var i = 0; i <= 30; i++){
+            shiftdetaildb.id = i
+            shiftdetaildb.date = date
+            if(date < 30){
+                date++
+            }else{
+                date = 1
+            }
+        }
+        
+        
+        let formula: String = worksheet.cellForCellReference("F6").stringValue()
 
         
         print(formula)
         
         
         
+    }
+    
+    //表中にあるスタッフ名の場所を返す
+    func BBB() -> Array<String>{
+        //let shiftdb = ShiftDB()
+        //let shiftdetaildb = ShiftDetailDB()
+        let documentPath: String = NSBundle.mainBundle().pathForResource("bbb", ofType: "xlsx")!
+        let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(documentPath)
+        let worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
+        
+        
+        let staffnumber = 27    //TODO: 仮に設定。あとで入力項目を設ける
+        let mark = "F"
+        var array:[String] = []
+        var number = 6
+        
+        while(true){
+            let Fcell: String = worksheet.cellForCellReference(mark+String(number)).stringValue()
+            if(Fcell.isEmpty){       //セルが空なら進めるだけ
+                number++
+            }else{
+                array.append(mark+String(number))
+                number++
+            }
+            
+            
+            if(staffnumber == array.count){       //設定したスタッフ人数と取り込み数が一致したら
+                break
+            }
+        }
+        return array
     }
 
 }
