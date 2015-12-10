b8dab382bb4072ff2996b891f733fee26d403233
Kenta Iwami <b1013068@fun.ac.jp>
2015-12-10 14:26:34 +0900

    メニューバーの配置と色の調整をした

--- masamon/masamon/Menu.swift
+++ masamon/masamon/Menu.swift
@@ -13,23 +13,31 @@ class Menu: UIViewController {
     let menuimage = UIImage(named: "../images/Menu-50_White.png")
     let MenuButton   = UIButton()
     var ToolBar = UIToolbar()
+    var statusbar = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //ステータスバーに被せるviewの作成
+        statusbar.frame = CGRectMake(0.0, 0.0, self.view.frame.width, 20.0)
+        statusbar.backgroundColor = UIColor.whiteColor()
+        
+        
         //ツールバーの作成
-        ToolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: 140.0))
-        ToolBar.layer.position = CGPoint(x: self.view.bounds.width/2, y: 0.0)
+        ToolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: 60.0))
+        ToolBar.layer.position = CGPoint(x: self.view.bounds.width/2, y: 50.0)
         ToolBar.barStyle = .BlackTranslucent
         ToolBar.tintColor = UIColor.whiteColor()
-        ToolBar.backgroundColor = UIColor.grayColor()
+        ToolBar.backgroundColor = UIColor.blackColor()
+        ToolBar.alpha = 0.1
         
         //メニューボタンの作成
         MenuButton.frame = CGRectMake(0, 0, 50, 50)
-        MenuButton.layer.position = CGPoint(x: self.view.frame.width-30, y:43)
+        MenuButton.layer.position = CGPoint(x: self.view.frame.width-30, y:50)
         MenuButton.setImage(menuimage, forState: .Normal)
         MenuButton.addTarget(self, action: "MenuButtontapped:", forControlEvents:.TouchUpInside)
         
+        self.view.addSubview(statusbar)
         self.view.addSubview(ToolBar)
         self.view.addSubview(MenuButton)
 
