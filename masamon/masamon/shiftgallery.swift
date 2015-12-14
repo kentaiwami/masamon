71eea61a73151577f5f38d6cd0525d5fad4ffb63
Kenta Iwami <b1013068@fun.ac.jp>
2015-12-15 01:59:24 +0900

    galleyに必要なファイルなどを追加

---
+++ masamon/masamon/shiftgallery.swift
@@ -0,0 +1,20 @@
+//
+//  shiftgallery.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2015/12/15.
+//  Copyright © 2015年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class shiftgallery: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
