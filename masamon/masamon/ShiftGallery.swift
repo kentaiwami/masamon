002877805df91d1cda211067351fefe3ed95af59
Kenta Iwami <b1013068@fun.ac.jp>
2015-12-15 10:27:42 +0900

    scrollviewのサンプルをコメントアウト

--- masamon/masamon/ShiftGallery.swift
+++ masamon/masamon/ShiftGallery.swift
@@ -15,32 +15,32 @@ class ShiftGallery: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // ScrollViewを生成.
-        myScrollView = UIScrollView()
-        
-        // ScrollViewの大きさを設定する.
-        myScrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
-        
-        // UIImageに画像を設定する.
-        let myImage = UIImage(named: "c014.jpg")!
-        
-        // UIImageViewを生成する.
-        let myImageView = UIImageView()
-        
-        // myImageViewのimageにmyImageを設定する.
-        myImageView.image = myImage
-        
-        // frameの値を設定する.
-        myImageView.frame = CGRectMake(0, 0, myImage.size.width, myImage.size.height)
-        
-        // ScrollViewにmyImageViewを追加する.
-        myScrollView.addSubview(myImageView)
-        
-        // ScrollViewにcontentSizeを設定する.
-        myScrollView.contentSize = CGSizeMake(myImageView.frame.size.width, myImageView.frame.size.height)
-        
-        // ViewにScrollViewをAddする.
-        self.view.addSubview(myScrollView)
+//        // ScrollViewを生成.
+//        myScrollView = UIScrollView()
+//        
+//        // ScrollViewの大きさを設定する.
+//        myScrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+//        
+//        // UIImageに画像を設定する.
+//        let myImage = UIImage(named: "c014.jpg")!
+//        
+//        // UIImageViewを生成する.
+//        let myImageView = UIImageView()
+//        
+//        // myImageViewのimageにmyImageを設定する.
+//        myImageView.image = myImage
+//        
+//        // frameの値を設定する.
+//        myImageView.frame = CGRectMake(0, 0, myImage.size.width, myImage.size.height)
+//        
+//        // ScrollViewにmyImageViewを追加する.
+//        myScrollView.addSubview(myImageView)
+//        
+//        // ScrollViewにcontentSizeを設定する.
+//        myScrollView.contentSize = CGSizeMake(myImageView.frame.size.width, myImageView.frame.size.height)
+//        
+//        // ViewにScrollViewをAddする.
+//        self.view.addSubview(myScrollView)
     }
     
     override func didReceiveMemoryWarning() {
