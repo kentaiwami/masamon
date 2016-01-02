71a59332e49e3d10fce1b5ecab39d69cbda41e18
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-02 23:30:39 +0900

    ・セルが選択された時にチェックマークを表示 ・セルの選択が解除された時にチェックマークを非表示

--- masamon/masamon/ShiftGalleryTable.swift
+++ masamon/masamon/ShiftGalleryTable.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class ShiftGalleryTable: UIViewController {
+class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var ButtomView: UIView!
+    
+    // セルに表示するテキスト
+    var shiftlist: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ButtomView.alpha = 0.8
 
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.allowsMultipleSelection = true
+
+        if(DBmethod().DBRecordCount(ShiftImportHistoryDB) != 0){
+            for(var i = DBmethod().DBRecordCount(ShiftImportHistoryDB)-1; i >= 0; i--){
+                let historydate = DBmethod().ShiftImportHistoryDBGet()[i].date
+                let historyname = DBmethod().ShiftImportHistoryDBGet()[i].name
+                shiftlist.append(historydate + "   " + historyname)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +42,31 @@ class ShiftGalleryTable: UIViewController {
     @IBAction func TapShowButton(sender: AnyObject) {
         let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("ShiftGallery")
         self.presentViewController( targetViewController, animated: true, completion: nil)
+    }
+    
+    // セルの行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shiftlist.count
+    }
+    
+    // セルの内容を変更
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = shiftlist[indexPath.row]
+        
+        return cell
+    }
+    
+    //セルが選択された時に呼ばれる
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableview.cellForRowAtIndexPath(indexPath)
+        cell?.accessoryType = UITableViewCellAccessoryType.Checkmark
+    }
+    
+    //セルの選択が解除された時に呼ばれる
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableview.cellForRowAtIndexPath(indexPath)
+        cell?.accessoryType = UITableViewCellAccessoryType.None
     }
 }
