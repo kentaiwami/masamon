402199aa3ae0c3cf50d45668f36e462fe5a69843
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-02 21:03:40 +0900

    必要なパーツを追加

---
+++ masamon/masamon/ShiftGalleryTable.swift
@@ -0,0 +1,24 @@
+//
+//  ShiftGalleryTable.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2016/01/02.
+//  Copyright © 2016年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class ShiftGalleryTable: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
