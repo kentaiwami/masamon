f01652750a27b66d5d37f805bfab4871295269aa
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-04 10:01:18 +0900

    ShiftGalleryTable内で表示の動作をするようにした 現在は、nodataを作業中

--- masamon/masamon/ShiftGalleryTable.swift
+++ masamon/masamon/ShiftGalleryTable.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import QuickLook
 
-class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDelegate,UICollectionViewDelegate, UICollectionViewDataSource,QLPreviewControllerDataSource{
 
     @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var ButtomView: UIView!
     
+    let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
+
     //  チェックされたセルの位置を保存しておく辞書をプロパティに宣言
     var selectedCells:[Bool]=[Bool]()
     
@@ -21,6 +24,8 @@ class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.AAA()
         
         ButtomView.alpha = 0.8
 
@@ -43,6 +48,13 @@ class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDel
     }
 
     override func viewWillAppear(animated: Bool) {
+        
+        if(DBmethod().DBRecordCount(ShiftImportHistoryDB) == 0){
+            no_dataimageview.alpha = 1.0
+        }else{
+            no_dataimageview.alpha = 0.0
+        }
+        
         shiftlist.removeAll()
         selectedCells.removeAll()
         
@@ -58,10 +70,18 @@ class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDel
         self.tableview.reloadData()
     }
     
+    override func viewDidAppear(animated: Bool) {
+        self.myCollectionView.reloadData()
+    }
+    
     //表示ボタンを押した時に呼ばれる関数
     @IBAction func TapShowButton(sender: AnyObject) {
         let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
         appDelegate.selectedcell = self.selectedCells
+        
+        myCollectionView.alpha = 1.0
+        self.view.bringSubviewToFront(myCollectionView)
+        self.myCollectionView.reloadData()
     }
     
     // セルの行数
@@ -97,5 +117,116 @@ class ShiftGalleryTable: UIViewController, UITableViewDataSource, UITableViewDel
         cell?.accessoryType = UITableViewCellAccessoryType.None
         
         selectedCells[indexPath.row] = false
+    }
+    
+    var myCollectionView: UICollectionView!
+    let no_dataimageview = UIImageView()
+    
+    func AAA(){
+        no_dataimageview.image = UIImage(named: "../no_data.png")
+        no_dataimageview.frame = CGRectMake(self.view.frame.width/2-250, self.view.frame.height/2-250, 500, 500)
+        if(DBmethod().DBRecordCount(ShiftImportHistoryDB) == 0){
+            no_dataimageview.alpha = 1.0
+        }else{
+            no_dataimageview.alpha = 0.0
+        }
+        
+        // CollectionViewのレイアウトを生成.
+        let layout = UICollectionViewFlowLayout()
+        
+        // Cell一つ一つの大きさ.
+        layout.itemSize = CGSizeMake(self.view.frame.width, 270)
+        
+        // Cellのマージン.
+        layout.sectionInset = UIEdgeInsetsMake(0, 0, 90, 0)
+        layout.minimumLineSpacing = 100.0
+        
+        // セクション毎のヘッダーサイズ.
+        layout.headerReferenceSize = CGSizeMake(100,30)
+        
+        // CollectionViewを生成.
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        // Cellに使われるクラスを登録.
+        myCollectionView.registerClass(CustomUICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        
+        myCollectionView.backgroundColor = UIColor.blackColor()
+        myCollectionView.alpha = 0.0
+        
+        let closeview = UIView()
+        closeview.frame = CGRectMake(0, no_dataimageview.frame.height-50, no_dataimageview.frame.width, 70)
+        closeview.backgroundColor = UIColor.hex("E6E6E6", alpha: 0.8)
+        
+        no_dataimageview.addSubview(closeview)
+        myCollectionView.addSubview(no_dataimageview)
+        
+        self.view.addSubview(myCollectionView)
+    }
+    
+    //Cellの総数を返す
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var count = 0
+        
+        for(var i = 0; i < appDelegate.selectedcell.count; i++){
+            if(appDelegate.selectedcell[i] == true){
+                count++
+            }
+        }
+        return count
+    }
+    
+    //Cellに値を設定する
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        var shiftlist: [String] = []
+        let count = DBmethod().DBRecordCount(ShiftImportHistoryDB)-1
+        
+        if(DBmethod().DBRecordCount(ShiftImportHistoryDB) != 0){
+            for(var i = 0; i <= count; i++){
+                if(appDelegate.selectedcell[i]){
+                    let historydate = DBmethod().ShiftImportHistoryDBGet()[count-i].date
+                    let historyname = DBmethod().ShiftImportHistoryDBGet()[count-i].name
+                    shiftlist.append(historydate + "     " + historyname)
+                }
+            }
+        }
+        
+        let cell : CustomUICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as! CustomUICollectionViewCell
+        
+        cell.textLabel?.text = shiftlist[indexPath.row]
+        cell.ql.dataSource = self
+        cell.ql.currentPreviewItemIndex = indexPath.row
+        
+        return cell
+    }
+    
+    //プレビューでの表示数
+    func numberOfPreviewItemsInPreviewController(controller: QLPreviewController) -> Int{
+        return 1
+    }
+    
+    //プレビューで表示するファイルの設定
+    func previewController(controller: QLPreviewController, previewItemAtIndex index: Int) -> QLPreviewItem{
+        var shiftlist: [String] = []
+        let count = DBmethod().DBRecordCount(ShiftImportHistoryDB)-1
+        
+        if(DBmethod().DBRecordCount(ShiftImportHistoryDB) != 0){
+            for(var i = 0; i <= count; i++){
+                if(appDelegate.selectedcell[i]){
+                    let historyname = DBmethod().ShiftImportHistoryDBGet()[count-i].name
+                    shiftlist.append(historyname)
+                }
+            }
+        }
+        
+        let Libralypath = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)[0] as String
+        let url = Libralypath + "/" + shiftlist[index]
+        
+        //        let documentPath: String = NSBundle.mainBundle().pathForResource("aaa", ofType: "xlsx")!
+        let doc = NSURL(fileURLWithPath: url)
+        return doc
     }
 }
