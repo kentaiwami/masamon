40382ddd243f0cabd0f89c84c6a1c7840e2120ec
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-08 07:07:53 +0900

    PDFmethod.swiftの追加

---
+++ masamon/masamon/PDFmethod.swift
@@ -0,0 +1,13 @@
+//
+//  PDFmethod.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2016/01/08.
+//  Copyright © 2016年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class PDFmethod: UIViewController {
+
+}
