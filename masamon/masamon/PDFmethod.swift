91a69d75cd452595b00f98a9ee0adbe3b7cf0da0
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-08 07:35:09 +0900

    ・TETに含まれていたsample.pdfを削除 ・PDFmethod内にメソッドを追加

--- masamon/masamon/PDFmethod.swift
+++ masamon/masamon/PDFmethod.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class PDFmethod: UIViewController {
-    let AAA = TET()
+    
+    func AAA(){
+        
+        let path: NSString
+        path = NSBundle.mainBundle().pathForResource("sample", ofType: "pdf")!
+        
+        let AAA = TET()
+        let BBB = AAA.open_document(path as String, optlist: "")
+        
+        print("BBB=>" + String(BBB))
+        
+        
+        let CCC = AAA.open_page(BBB, pagenumber: 1, optlist: "granularity=page")
+        
+        print("CCC=>" + String(CCC))
+        
+        
+        let DDD = AAA.get_text(CCC)
+        print(DDD)
+    }
 }
