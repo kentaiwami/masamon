4a7d684df5de696350a50d887b55da1e0c140cbf
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-08 14:32:07 +0900

    スタッフの人数分だけ取り込むように変更

--- masamon/masamon/PDFmethod.swift
+++ masamon/masamon/PDFmethod.swift
@@ -50,7 +50,7 @@ class PDFmethod: UIViewController {
         
         pdftext.enumerateLines{
             line, stop in
-
+            
             if(nowIndex < lineIndex){      //平成を見つけた行まで進める
                 nowIndex += 1
             }else{
@@ -63,21 +63,29 @@ class PDFmethod: UIViewController {
                 }
             }
         }
-
+        
         //スタッフの行を読み取る
         nowIndex = 0
+        var staffcount = 0
+        let TEST = 29           //スタッフ人数テスト用変数
         
         pdftext.enumerateLines{
             line, stop in
             
-            if(nowIndex < lineIndex){      //店長を見つけた行まで進める
-                nowIndex += 1
-            }else{
-                let judgehtopcharacter = line.substringToIndex(line.startIndex.successor())
-                
-                if(Int(judgehtopcharacter) != nil){         //先頭文字が数値の場合のみ
-                    print(line)
+            // if(staffcount <= DBmethod().StaffNumberGet()){
+            if(staffcount <= TEST){
+                if(nowIndex < lineIndex){      //店長を見つけた行まで進める
+                    nowIndex += 1
+                }else{
+                    let judgehtopcharacter = line.substringToIndex(line.startIndex.successor())
+                    
+                    if(Int(judgehtopcharacter) != nil){         //先頭文字が数値の場合のみ
+                        print(line)
+                        staffcount += 1
+                    }
                 }
+            }else{
+                stop = true
             }
         }
     }
