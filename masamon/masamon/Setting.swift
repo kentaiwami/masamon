4356e1fd694dcd41f0ce761fb1d6cfe1b34b7c7f
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-31 17:48:00 +0900

    設定画面で使用する画像ファイルを追加, ボタンを4つ追加

--- masamon/masamon/Setting.swift
+++ masamon/masamon/Setting.swift
@@ -10,19 +10,44 @@ import UIKit
 
 class Setting: UIViewController {
 
+
+    let buttonimages = ["../images/usersetting.png","../images/staffnamelist.png","../images/shiftnamelist.png","../images/shiftlist.png"]
+    let buttonpositon: [[Int]] = [[80,160],[280,160],[90,380],[280,380]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        //画面遷移するためのボタンを追加
+        for(var i = 0; i < 4; i++){
+            let button = UIButton()
+            
+            button.tag = i + 1
+            button.setImage(UIImage(named: buttonimages[i]), forState: .Normal)
+            button.frame = CGRectMake(0, 0, 100, 100)
+            button.layer.position = CGPoint(x: buttonpositon[i][0], y:buttonpositon[i][1])
+            button.addTarget(self, action: "Buttontapped:", forControlEvents:.TouchUpInside)
+            self.view.addSubview(button)
+        }
     }
     
-    
-    @IBAction func TapUserSetting(sender: AnyObject) {
-        
-        
-        let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("UserSetting")
-        self.presentViewController( targetViewController, animated: true, completion: nil)
+    func Buttontapped(sender:UIButton){
+        switch(sender.tag){
+        case 1:
+            let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("UserSetting")
+            self.presentViewController( targetViewController, animated: true, completion: nil)
+            
+        case 2:
+            print("2")
+            
+        case 3:
+            print("3")
+            
+        case 4:
+            print("4")
+            
+        default:
+            break
 
-        
+        }
     }
 }
