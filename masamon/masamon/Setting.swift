7bbe052ff13a7a0708f686e5b3cf9cca6364d809
Kenta Iwami <b1013068@fun.ac.jp>
2016-01-31 18:47:35 +0900

    設定画面にラベルを追加

--- masamon/masamon/Setting.swift
+++ masamon/masamon/Setting.swift
@@ -13,20 +13,21 @@ class Setting: UIViewController {
 
     let buttonimages = ["../images/usersetting.png","../images/staffnamelist.png","../images/shiftnamelist.png","../images/shiftlist.png"]
     let buttonpositon: [[Int]] = [[80,160],[280,160],[90,380],[280,380]]
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //画面遷移するためのボタンを追加
         for(var i = 0; i < 4; i++){
-            let button = UIButton()
             
+            //画面遷移するためのボタンを追加
+            let button = UIButton()
             button.tag = i + 1
             button.setImage(UIImage(named: buttonimages[i]), forState: .Normal)
             button.frame = CGRectMake(0, 0, 100, 100)
             button.layer.position = CGPoint(x: buttonpositon[i][0], y:buttonpositon[i][1])
             button.addTarget(self, action: "Buttontapped:", forControlEvents:.TouchUpInside)
             self.view.addSubview(button)
+            
         }
     }
     
