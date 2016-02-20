298f3471a6757265da73a07b67c986562148dbf1
Kenta Iwami <b1013068@fun.ac.jp>
2016-02-20 19:17:36 +0900

    ビデオの再生ができた

--- masamon/masamon/Video.swift
+++ masamon/masamon/Video.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVKit
 
 class Video: UIViewController {
+
+    var myMoviePlayerView : AVPlayerViewController!
+    let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,20 +21,41 @@ class Video: UIViewController {
         let position = [-120,180]
         
         for(var i = 0; i < 2; i++){
-            let view = UIButton()
+            let thumbnailbutton = UIButton()
             let image = UIImage(named: imagepath[i])
-            view.setImage(image, forState: .Normal)
-            view.frame = CGRectMake(0, 0, 300, 200)
-            view.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2+CGFloat(position[i]))
-            view.contentMode = UIViewContentMode.ScaleAspectFit
+            thumbnailbutton.setImage(image, forState: .Normal)
+            thumbnailbutton.frame = CGRectMake(0, 0, 300, 200)
+            thumbnailbutton.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2+CGFloat(position[i]))
+            thumbnailbutton.contentMode = UIViewContentMode.ScaleAspectFit
+            thumbnailbutton.tag = i+1
+            thumbnailbutton.addTarget(self, action: "TapThumbnail:", forControlEvents: .TouchUpInside)
             
-            self.view.addSubview(view)
+            //2つ目の動画を作成していないため、ボタンを無効化する
+            if(i == 1){
+                thumbnailbutton.enabled = false
+            }
+            
+            self.view.addSubview(thumbnailbutton)
         }
         
     }
 
     @IBAction func TapBackButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func TapThumbnail(sender: UIButton) {
+        switch(sender.tag){
+        case 1:
+            appDelegate.thumbnailnumber = 1
+            
+        default:
+            appDelegate.thumbnailnumber = 2
+            break
+        }
+        
+        let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("VideoViewController")
+        self.presentViewController( targetViewController, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
