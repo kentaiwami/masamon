2932b9ea0c73a9a0358a958eb5fa90c246d0b99b
Kenta Iwami <b1013068@fun.ac.jp>
2016-05-05 14:08:32 +0900

    設定画面にtableviewを追加した

--- masamon/masamon/Setting.swift
+++ masamon/masamon/Setting.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class Setting: UIViewController {
+class Setting: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
     let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableview.delegate = self
+        tableview.dataSource = self
+        
     }
     
     override func viewDidAppear(animated: Bool) {
         appDelegate.screennumber = 2
+    }
+    
+    // セルに表示するテキスト
+    let texts = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    // セルの行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+    // セルの内容を変更
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = texts[indexPath.row]
+        return cell
     }
 }
