5479e43d1d3082a5b4bd45143f8e2ea699b804d1
Kenta Iwami <b1013068@fun.ac.jp>
2016-05-05 14:00:15 +0900

    設定画面のアイコン，ボタン，ラベルを削除した

--- masamon/masamon/Setting.swift
+++ masamon/masamon/Setting.swift
@@ -10,53 +10,14 @@ import UIKit
 
 class Setting: UIViewController {
 
-
-    let buttonimages = ["../images/usersetting.png","../images/staffnamelist.png","../images/shiftnamelist.png","../images/shiftlist.png"]
-    let buttonpositon: [[Int]] = [[90,160],[280,160],[100,380],[280,380]]
     let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 0 ..< 4{
-            
-            //画面遷移するためのボタンを追加
-            let button = UIButton()
-            button.tag = i + 1
-            button.setImage(UIImage(named: buttonimages[i]), forState: .Normal)
-            button.frame = CGRectMake(0, 0, 100, 100)
-            button.layer.position = CGPoint(x: buttonpositon[i][0], y:buttonpositon[i][1])
-            button.addTarget(self, action: #selector(Setting.Buttontapped(_:)), forControlEvents:.TouchUpInside)
-            self.view.addSubview(button)
-            
-        }
     }
     
     override func viewDidAppear(animated: Bool) {
         appDelegate.screennumber = 2
-    }
-    
-    func Buttontapped(sender:UIButton){
-        switch(sender.tag){
-        case 1:
-            let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("UserSetting")
-            self.presentViewController( targetViewController, animated: true, completion: nil)
-            
-        case 2:
-            let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("StaffNameListSetting")
-            self.presentViewController( targetViewController, animated: true, completion: nil)
-            
-        case 3:
-            let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("ShiftNameListSetting")
-            self.presentViewController( targetViewController, animated: true, completion: nil)
-            
-        case 4:
-            let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("ShiftListSetting")
-            self.presentViewController( targetViewController, animated: true, completion: nil)
-            
-        default:
-            break
-
-        }
     }
 }
