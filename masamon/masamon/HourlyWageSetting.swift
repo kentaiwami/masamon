e572ed0f6cae5e91e6e6dc6f9864d851b4777ae6
Kenta Iwami <b1013068@fun.ac.jp>
2016-05-08 04:04:45 +0900

    パーツの位置を調整して不要な記述を削除した

--- masamon/masamon/HourlyWageSetting.swift
+++ masamon/masamon/HourlyWageSetting.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,UITextFieldDelegate{
     
-    @IBOutlet weak var AddScrollView: UIScrollView!
     @IBOutlet weak var TimeFrom1: UITextField!
     @IBOutlet weak var TimeTo1: UITextField!
     @IBOutlet weak var TimeFrom2: UITextField!
@@ -34,23 +33,19 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     let saveimage = UIImage(named: "../images/save.png")
     let savebutton   = UIButton()
     
-//    let catimagepath: [String] = ["../images/cat1.png","../images/cat2.png"]
     let catinfo: [[Int]] = [[70,620,80],[326,470,80]]
     
-    let frameborder: [Int] = [45,220]
+    let frameborder: [Int] = [90,280]
     
-    let clock: [Int] = [65,235]
-    let yen: [Int] = [125,295]
-    let user: [Int] = [460,515]
-//    let usericonfilename: [String] = ["../images/user.png","../images/user2.png"]
+    let clock: [Int] = [115,300]
+    let yen: [Int] = [175,360]
     
-    @IBOutlet weak var HPSView: UIView!
     var txtActiveField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.HPSView.backgroundColor = UIColor.blackColor()
+        self.view.backgroundColor = UIColor.blackColor()
         
         SetText()
       
@@ -62,8 +57,8 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
             frameborderline.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.4).CGColor
             frameborderline.layer.borderWidth = 2
             frameborderline.layer.cornerRadius = 30
-            self.HPSView.addSubview(frameborderline)
-            self.HPSView.sendSubviewToBack(frameborderline)
+            self.view.addSubview(frameborderline)
+            self.view.sendSubviewToBack(frameborderline)
         }
 
         //時計アイコンの設置
@@ -71,7 +66,7 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
             let clockicon = UIImageView()
             clockicon.image = UIImage(named: "../images/clock.png")
             clockicon.frame = CGRectMake(24, CGFloat(clock[i]), 42, 40)
-            self.HPSView.addSubview(clockicon)
+            self.view.addSubview(clockicon)
         }
         
         //円アイコンの設置
@@ -79,7 +74,7 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
             let yenicon = UIImageView()
             yenicon.image = UIImage(named: "../images/yen.png")
             yenicon.frame = CGRectMake(24, CGFloat(yen[i]), 42, 40)
-            self.HPSView.addSubview(yenicon)
+            self.view.addSubview(yenicon)
         }
         
 //        //シフト関連のアイコンを設置
@@ -90,24 +85,11 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
 //            self.HPSView.addSubview(usericon)
 //
 //        }
-        
-//        //猫の追加
-//        for i in 0 ..< catimagepath.count{
-//            let catimage = UIImage(named: catimagepath[i])
-//            let catimageview = UIImageView()
-//            
-//            catimageview.frame = CGRectMake(0, 0, CGFloat(catinfo[i][2]), CGFloat(catinfo[i][2]))
-//            catimageview.image = catimage
-//            catimageview.layer.position = CGPoint(x: catinfo[i][0], y: catinfo[i][1])
-//            
-//            self.HPSView.addSubview(catimageview)
-//            
-//        }
-        
+                
         //セーブボタンの追加
         savebutton.tag = 0
         savebutton.frame = CGRectMake(0, 0, 70, 70)
-        savebutton.layer.position = CGPoint(x: self.view.frame.width/2, y:480)
+        savebutton.layer.position = CGPoint(x: self.view.frame.width/2, y:500)
         savebutton.setImage(saveimage, forState: .Normal)
         savebutton.addTarget(self, action: #selector(HourlyWageSetting.SaveButtontapped(_:)), forControlEvents:.TouchUpInside)
         self.view.addSubview(savebutton)
@@ -369,34 +351,7 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         view.endEditing(true)
         return true
     }
-    
-    func handleKeyboardWillShowNotification(notification: NSNotification) {
         
-        let userInfo = notification.userInfo!
-        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-        let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
-        
-        let txtLimit = txtActiveField.frame.origin.y + txtActiveField.frame.height + 70.0
-        let kbdLimit = myBoundSize.height - keyboardScreenEndFrame.size.height
-                
-        if txtLimit >= kbdLimit {
-            AddScrollView.contentOffset.y = txtLimit - kbdLimit
-        }
-    }
-    
-    func handleKeyboardWillHideNotification(notification: NSNotification) {
-        AddScrollView.contentOffset.y = 0
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-                
-        super.viewWillAppear(animated)
-        
-        let notificationCenter = NSNotificationCenter.defaultCenter()
-        notificationCenter.addObserver(self, selector: #selector(HourlyWageSetting.handleKeyboardWillShowNotification(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(HourlyWageSetting.handleKeyboardWillHideNotification(_:)), name: UIKeyboardWillHideNotification, object: nil)
-    }
-    
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
         
@@ -407,9 +362,6 @@ class HourlyWageSetting: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         SetText()
     }
     
-//    func TapToolBarButton(sender: UIButton){
-//        staffnumbertextfield.resignFirstResponder()
-//    }
     
     func SetText(){
         //既に登録されていたら登録内容を表示する
