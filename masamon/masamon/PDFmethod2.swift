b4317e610a82607f5b6a7a9d69dbc923896591fc
Kenta Iwami <b1013068@fun.ac.jp>
2016-08-13 14:29:16 +0900

    PDFmethod2.swiftを作成

---
+++ masamon/masamon/PDFmethod2.swift
@@ -0,0 +1,22 @@
+//
+//  PDFmethod2.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2016/08/13.
+//  Copyright © 2016年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class PDFmethod2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
