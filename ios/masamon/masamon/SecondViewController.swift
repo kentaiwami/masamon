0f7685b36f3244cf36e0d0b5532ff76920edeceb
Kenta Iwami <g2117009@fun.ac.jp>
2018-02-09 12:42:17 +0900

    前のバージョンを削除、新規プロジェクト作成

---
+++ ios/masamon/masamon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  masamon
+//
+//  Created by 岩見建汰 on 2018/02/09.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
