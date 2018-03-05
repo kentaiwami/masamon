c4657e4d8195a1f507d7facda052c0f45b94aac4
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-06 00:20:58 +0900

    従業員テーブルを作成

--- ParJob_API/ParJob/models/models.py
+++ ParJob_API/ParJob/models/models.py
@@ -4,9 +4,17 @@ from ParJob.database import db
 
 class User(db.Model):
 
-    __tablename__ = 'users'
+    __tablename__ = 'Employee'
 
     id = db.Column(db.Integer, primary_key=True)
-    hoge = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    code = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    daytime_start = db.Column(db.Time, nullable=True)
+    daytime_end = db.Column(db.Time, nullable=True)
+    daytime_hourly_wage = db.Column(db.Integer, nullable=True)
+    night_start = db.Column(db.Time, nullable=True)
+    night_end = db.Column(db.Time, nullable=True)
+    night_hourly_wage = db.Column(db.Integer, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
