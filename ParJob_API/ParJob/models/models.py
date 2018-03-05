9e4754409890b7171c97123953166b5791f3c776
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-05 16:40:22 +0900

    マイグレーションの設定を追加

--- ParJob_API/ParJob/models/models.py
+++ ParJob_API/ParJob/models/models.py
@@ -7,6 +7,6 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    hoge = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
