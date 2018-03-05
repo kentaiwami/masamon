5c2ff435be370d4af1a9d5080adb7a389575c89e
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-06 03:18:50 +0900

    update table name

--- ParJob_API/ParJob/models/models.py
+++ ParJob_API/ParJob/models/models.py
@@ -2,17 +2,17 @@ from datetime import datetime
 from ParJob.database import db
 
 class Company(db.Model):
-    __tablename__ = 'Company'
+    __tablename__ = 'company'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
     code = db.Column(db.String(255), nullable=False, unique=True)
 
-    employees = db.relationship('Employee', backref='Company', lazy='dynamic')
+    employees = db.relationship('Employee', backref='company', lazy='dynamic')
 
 
 class Employee(db.Model):
-    __tablename__ = 'Employee'
+    __tablename__ = 'employee'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
@@ -24,6 +24,6 @@ class Employee(db.Model):
     night_start = db.Column(db.Time, nullable=True)
     night_end = db.Column(db.Time, nullable=True)
     night_hourly_wage = db.Column(db.Integer, nullable=True)
-    company_id = db.Column(db.Integer, db.ForeignKey('Company.id'))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
