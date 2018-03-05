a3a30b3630ab6c9e7407c8437731801bfc01ff67
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-06 01:09:32 +0900

    companyとemployeeのリレーション作成

--- ParJob_API/ParJob/models/models.py
+++ ParJob_API/ParJob/models/models.py
@@ -1,9 +1,17 @@
 from datetime import datetime
 from ParJob.database import db
 
+class Company(db.Model):
+    __tablename__ = 'Company'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    code = db.Column(db.String(255), nullable=False)
+
+    employees = db.relationship('Employee', backref='Company', lazy='dynamic')
+
 
 class Employee(db.Model):
-
     __tablename__ = 'Employee'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,5 +24,6 @@ class Employee(db.Model):
     night_start = db.Column(db.Time, nullable=True)
     night_end = db.Column(db.Time, nullable=True)
     night_hourly_wage = db.Column(db.Integer, nullable=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('Company.id'))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
