a771177f1a8ce4326d72bd7d8aacab1c83dca3a0
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-12 11:56:33 +0900

    app builderの導入

--- ParJob_API/ParJob/database.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-
-
-def init_db(app):
-    db.init_app(app)
-    Migrate(app, db)
--- ParJob_API/ParJob/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import *
-
-
-__all__ = [
-]
