713e682e162d6a8be3b1872888643971c87a49f6
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-17 12:00:09 +0900

    regist auth

--- API/view/__init__.py
+++ API/view/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from .api import *
+from .auth import *
 
 application = Flask(__name__)
 
-modules_define = [api.app]
+modules_define = [api.app, auth.app]
 for app in modules_define:
     application.register_blueprint(app)
