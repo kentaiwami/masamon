6704388bf9a6aa95f8263e14836f4f616da683a8
Kenta Iwami <g2117009@fun.ac.jp>
2018-03-23 16:01:25 +0900

    add test relation

--- API/model.py
+++ API/model.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from database import db
 from sqlalchemy import String, Integer
 
@@ -8,10 +9,12 @@ class Thing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item1 = db.Column(String(10))
     item2 = db.Column(String(20))
+    book_id = db.Column(Integer, db.ForeignKey('book.id'))
 
 
-# class Book(db.Model):
-#     __tablename__ = 'book'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(String(20))
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(String(20))
+    things = db.relationship("Thing", backref="book", cascade='all, delete-orphan')
